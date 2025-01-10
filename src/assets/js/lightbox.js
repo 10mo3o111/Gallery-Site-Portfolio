6325fabd2073a04ed3d172fd14051b40d222a9f2
tomomi <t2024ogawa@gmail.com>
2025-01-10 10:26:18 +0900

    不要なディレクトリを削除

--- src/assets/js/lightbox.js
+++
@@ -1,9 +0,0 @@
-import "lightbox2/dist/css/lightbox.css";
-import lightbox from "lightbox2/dist/js/lightbox.js";
-
-lightbox.option({
-  resizeDuration: 200,
-  wrapAround: true,
-  disableScrolling: true,
-  fitImagesInViewport: true,
-});
